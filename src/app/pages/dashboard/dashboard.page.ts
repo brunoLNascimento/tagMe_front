070bee747991fc645544b43927e554686e1c7b7b
brunoLNascimento <bnascimento1@live.com>
2019-06-25 19:39:12 -0300

    consulta por nome prato e ajustes css

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingService } from 'src/app/loading/loading.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  res: any;
+  return: any;
+  searchQuery: any;
+  constructor( 
+    public loading: LoadingService,
+    private http: HttpClient,
+    private toastController: ToastController,
+  ) { }
 
   ngOnInit() {
+    this.loading.present();
+    this.http.get("http://localhost:4000/tagMe/menu").subscribe(res =>{
+      this.loading.dismiss();
+      this.return = res
+    })
+  }
+
+  updateList() {
+    this.http.get(`http://localhost:4000/tagMe/menu/${this.searchQuery}`)
+    .subscribe( async (response: any) => {
+      this.return = response
+    },(error: any) => {
+      this.toast(error.error.message, 'danger');
+    });
+  }
+
+  async toast(msg, color) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 3000,
+      color: color
+    });
+    toast.present();
   }
 
 }
