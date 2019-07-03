fc64bf145427b64d42aec2f35918e10ef3d800fd
brunoLNascimento <bnascimento1@live.com>
2019-07-03 17:57:27 -0300

    removendo tela home

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -13,6 +13,7 @@ export class DashboardPage implements OnInit {
   res: any;
   return: any;
   searchQuery: any;
+  usuario: any;
 
   constructor( 
     public loading: LoadingService,
@@ -34,9 +35,20 @@ export class DashboardPage implements OnInit {
   updateList() {
     this.http.get(`http://localhost:3000/tagMe/menu/${this.searchQuery}`)
     .subscribe( async (response: any) => {
-  //    this.storage.get('details').then(details  => {
+    //this.storage.get('menu').then(menu  => {
       this.return = response
-      //this.return = details
+     
+  /*  if(this.searchQuery){
+      this.return.foreach(function(nomePrato){
+        this.searchQuery = new RegExp(this.searchQuery)
+       var teste = this.searchQuery.exec(nomePrato)
+       this.return = teste
+
+      })
+    }else{
+      this.return = menu
+    }*/
+
     },(error: any) => {
       this.toast(error.error.message, 'danger');
     });
@@ -44,11 +56,16 @@ export class DashboardPage implements OnInit {
 
   details(dish){
     this.storage.set('details', dish)
-    this.nav.navigateForward(['details'])
+   this.nav.navigateForward(['details'])
   }
 
   logout(){
     this.storage.clear()
+    this.usuario = {
+      "nome": "usuarioTeste",
+      "senha": "senhaUsuario"
+    }
+    this.storage.set("usuario", this.usuario) 
     this.nav.navigateForward(['login'])
   }
 
